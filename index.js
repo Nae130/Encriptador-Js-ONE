4f944f7bca66e4ffef4f21e86a8fa590ac5476e8
Juan <a@a.com>
2023-03-06 00:24:48 -0600

    Terminando el challenge

--- index.js
+++ index.js
@@ -1,38 +1,47 @@
-let textoEncriptado=getElementById('area-principal');
-const encriptadas={
-    'a':'ai',
-    'e':'enter',
-    'i':'imes',
-    'o':'ober',
-    'u':'ufat'
-}
-const desencriptadas={
-    'ai':'a',
-    'enter':'e',
-    'imes':'i',
-    'ober':'o',
-    'ufat':'u'
-}
-
-function encriptar{
-    let mensaje=textoEncriptado.value; 
-    if(mensaje==''){
-        alert('Agrega un mensaje para poder continuar');
-    }else{
-        for(i in encriptadas){
-            mensaje=mensaje.value
-        }
-    }
-}
+const textoAEncriptar=document.getElementById('area-principal'),
+    btCopiar=document.getElementById('bt-copiar'),
+    mensajeP=document.getElementById('mensaje-p'),
+    mensajeImagen=document.getElementById('mensaje-img'),
+    mensajeC=document.getElementById('mensaje-c'),
+    textoDesEn=document.getElementById('textoDes-En'),
+    btEncriptar=document.getElementById('bt-encriptar'),
+    btDesencriptar=document.getElementById('bt-desencriptar');     
+btCopiar.addEventListener('click',function(){
+  textoDesEn.select();
+  document.execCommand('copy');
+  alert('TextoCopiado');
+});
 
 
-
-
-
-
-
-
-
-
-let mensaje=document.getElementById('area-principal')
-
+btEncriptar.addEventListener("click", function () {
+  let texto = textoAEncriptar.value;
+  if (texto != "") {
+    texto = texto.replace(/e/g, "enter");
+    texto = texto.replace(/i/g, "imes");
+    texto = texto.replace(/a/g, "ai");
+    texto = texto.replace(/o/g, "ober");
+    texto = texto.replace(/u/g, "ufat");
+    textoDesEn.value = texto;
+    mensajeImagen.style.display = "none";
+    mensajeC.style.display = "none";
+    mensajeP.style.display="none";
+    btCopiar.style.display = "block";
+    textoDesEn.style.display = "block";
+  }
+});
+btDesencriptar.addEventListener("click", function () {
+  let texto = textoAEncriptar.value;
+  if (texto != "") {
+    texto = texto.replace(/enter/g, "e");
+    texto = texto.replace(/imes/g, "i");
+    texto = texto.replace(/ai/g, "a");
+    texto = texto.replace(/ober/g, "o");
+    texto = texto.replace(/ufat/g, "u");
+    textoDesEn.value = texto;
+    mensajeImagen.style.display = "none";
+    mensajeC.style.display = "none";
+    mensajeP.style.display="none";
+    btCopiar.style.display = "block";
+    textoDesEn.style.display = "block";
+  }
+});
