3876d574f7ea0c913316437358d09c4a51ef1431
Juan <a@a.com>
2023-03-06 00:20:24 -0600

    Terminando el challenge

--- index.js
+++ index.js
@@ -1,47 +1,38 @@
-function Encriptar(){
-    for (let i = 0; i=mensaje.value.length; i++){
-        if (mensaje.value == undefined){
-            alert ('Agrega un mensaje para poder continuar')
-        }
-        else if (mensaje.value[i]='a'){
-            nuevomensaje[i]='a';
-            nuevomensaje[i++]='i'
-        }else if(mensaje.value[i]='e'){
-            nuevomensaje[i]='e';
-            nuevomensaje[i++]='n';
-            nuevomensaje[i++]='t';
-            nuevomensaje[i++]='e';
-            nuevomensaje[i++]='r';
-        }else if(mensaje.value[i]='i'){
-            nuevomensaje[i]='i';
-            nuevomensaje[i++]='m';
-            nuevomensaje[i++]='e';
-            nuevomensaje[i++]='s';          
-        }else if(mensaje.value[i]='o'){
-            nuevomensaje[i]='o';
-            nuevomensaje[i]='b';
-            nuevomensaje[i]='e';
-            nuevomensaje[i]='r';
-        }else if(mensaje.value[i]='u'){
-            nuevomensaje[i]='u';
-            nuevomensaje[i]='f';
-            nuevomensaje[i]='a';
-            nuevomensaje[i]='t';
-        }
-        nuevomensaje[i]= mensaje.value[i];
-    } 
-    alert(nuevomen  );
+let textoEncriptado=getElementById('area-principal');
+const encriptadas={
+    'a':'ai',
+    'e':'enter',
+    'i':'imes',
+    'o':'ober',
+    'u':'ufat'
+}
+const desencriptadas={
+    'ai':'a',
+    'enter':'e',
+    'imes':'i',
+    'ober':'o',
+    'ufat':'u'
 }
 
-function Desencriptar(){
-    alert('desencriptar')
+function encriptar{
+    let mensaje=textoEncriptado.value; 
+    if(mensaje==''){
+        alert('Agrega un mensaje para poder continuar');
+    }else{
+        for(i in encriptadas){
+            mensaje=mensaje.value
+        }
+    }
 }
 
 
 
 
-let mensaje =document.querySelector('textarea')
-let nuevomensaje='         ';   
-mensaje.focus();
-let encriptar =document.querySelector('encriptar')
-let desencriptar=document.querySelector('desencriptar')
+
+
+
+
+
+
+let mensaje=document.getElementById('area-principal')
+
